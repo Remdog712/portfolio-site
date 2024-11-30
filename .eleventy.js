36a4cdd1d222f97e5c9a39c572b2f3d52acd04e8
Remington Orange <remingtonorange@yahoo.com>
2024-11-30 01:33:29 -0500

    Minor tweak

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./_src/css/");
-    eleventyConfig.addPassthroughCopy("./_src/js/");
-    eleventyConfig.addPassthroughCopy("./_src/images/");
-    eleventyConfig.addPassthroughCopy("./_src/models/");
-    eleventyConfig.addPassthroughCopy("./_src/_data/");
-    eleventyConfig.addPassthroughCopy("./_src/RO_Web.ico");
-    
-    return {
-      dir: {
-        input: "_src",
-        includes: "_includes",
-        output: "_site",
-      },
-    };
+  eleventyConfig.addPassthroughCopy("./_src/css/");
+  eleventyConfig.addPassthroughCopy("./_src/js/");
+  eleventyConfig.addPassthroughCopy("./_src/images/");
+  eleventyConfig.addPassthroughCopy("./_src/models/");
+  eleventyConfig.addPassthroughCopy("./_src/_data/");
+  eleventyConfig.addPassthroughCopy("./_src/RO_Web.ico");
+
+  return {
+    pathPrefix: "/portfolio-site/", 
+    dir: {
+      input: "_src",
+      includes: "_includes",
+      output: "_site",
+    },
   };
-  
+};
